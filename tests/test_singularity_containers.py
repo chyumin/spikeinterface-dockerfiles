fcfa3cc9e7b11242f94fd9f4ade7ebb99d8e5cf8
Chuang Yu Min <chg.yumin@gmail.com>
2022-08-05 10:54:13 -0300

    tridesclous final test

--- tests/test_singularity_containers.py
+++ tests/test_singularity_containers.py
@@ -48,8 +48,9 @@ def test_mountainsort4(run_kwargs):
 
 
 def test_tridesclous(run_kwargs):
-    run_kwargs['singularity_image'] = 'chyumin/tridesclous-base:latest'
-    sorting = ss.run_tridesclous(output_folder="tridesclous", **run_kwargs)
+    run = run_kwargs.copy()
+    run['singularity_image'] = 'chyumin/tridesclous-base:latest'
+    sorting = ss.run_tridesclous(output_folder="tridesclous", **run)
     print(sorting)
 
 
